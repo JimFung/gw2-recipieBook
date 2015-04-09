cf61a9b5e2753fcc517e1e802facd40355e098d5
JimFung <jfung92@gmail.com>
2015-04-08 21:51:06 -0700

    Researching data set creation
    Added MIT License
    updated gitignore
    started on logic for creating data set

--- js/search.js
+++ js/search.js
@@ -1,6 +1,27 @@
 var url = 'https://api.guildwars2.com/v2/'
 
+/*
+https://api.guildwars2.com/v2/items "name"
+https://api.guildwars2.com/v2/recipes "output_item_id"
+*/
+var master_list = [];
 $(document).ready(function(){
+
+	/*Set-up data*/
+	$.get(url+"recipes", function(data){
+		console.log(data.length);
+		for(var i = 0, len = data.length; i < len; i++){
+			$.get(url+"recipes/"+data[i], function(reci){
+				$.get(url+"items/"+reci["output_item_id"],function(item){
+					// console.log(item["name"] + ": " + reci["output_item_id"]);
+					// $(".content").append("<p>\"" + item["name"] + "\": " + reci["output_item_id"] + ",</p>");
+					master_list[item["name"]] = reci["output_item_id"];					
+				});
+			});
+		}
+	});
+
+	console.log(master_list);
 
 	// submit on enter
 	$(".search-box").keypress(function(event){
@@ -17,6 +38,7 @@ $(document).ready(function(){
 
 	// search logic
 	function search(recipe){
-		
+
 	}
 });
+
